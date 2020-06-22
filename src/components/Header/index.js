d316625f4a3e01317d2f8ad073d6284d34a62bd2
sanye <liubingjian@hzyushi.cn>
2020-06-22 10:08:43 +0800

    add: 添加头部组件

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import {Row, Col} from 'antd'
+import './index.less'
+import Util from '../../utils/utils'
 export default class Header extends React.Component {
+    state = {
+    }
+    componentWillMount () {
+        this.setState({
+            userName: '三叶'
+        })
+        setInterval(() => {
+            let systime = Util.formateDate(new Date().getTime())
+            this.setState({
+                systime
+            })
+        }, 1000)
+    }
     render() {
         return (
-            <div>header</div>
+            <div className='header'>
+                <Row className='header-top'>
+                    <Col span='24'>
+                        <span>欢迎，{this.state.userName}</span>
+                        <a href='javascript;'>退出</a>
+                    </Col>
+                </Row>
+                <Row className='breadcrumb'>
+                    <Col span='4' className='breadcrumb-title'>
+                        首页
+                    </Col>
+                    <Col span='20' className='weather'>
+                        <span className='date'>{this.state.systime}</span>
+                        <span>晴转多云</span>
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
